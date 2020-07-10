e6384f284e28444896d0d3bb44d6f013243be0a7
YC <aa>
2020-07-11 04:15:03 +0800

    Notify-011: Move to Util

--- app/src/main/java/github/daneren2005/dsub/dialogFragment/NotifyDialogFragmentTemplate.java
+++ app/src/main/java/github/daneren2005/dsub/dialogFragment/NotifyDialogFragmentTemplate.java
@@ -14,6 +14,7 @@ import android.view.WindowManager;
 import android.widget.ImageButton;
 
 import github.daneren2005.dsub.R;
+import github.daneren2005.dsub.util.Util;
 
 public class NotifyDialogFragmentTemplate extends AppCompatDialogFragment {
 
@@ -52,14 +53,7 @@ public class NotifyDialogFragmentTemplate extends AppCompatDialogFragment {
                 window.addFlags(WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE);
                 getDialog().setOnShowListener((dialog) -> {
                     window.clearFlags(WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE);
-                    window.getDecorView().setSystemUiVisibility(
-                            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                                    | View.SYSTEM_UI_FLAG_FULLSCREEN
-                                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                    );
+                    Util.toImmersiveMode(window);
                 });
             }
         }
--- app/src/main/java/github/daneren2005/dsub/util/Util.java
+++ app/src/main/java/github/daneren2005/dsub/util/Util.java
@@ -52,6 +52,8 @@ import android.util.Log;
 import android.util.SparseArray;
 import android.view.View;
 import android.view.Gravity;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -1520,5 +1522,25 @@ public final class Util {
 		}
 
 		return random;
+	}
+
+	/*
+	 * Util for hide status bar and navigation bar.
+	 * @param Window, from getWindow() on Activity or Dialog
+	 * For Activity, call it on onWindowFocusChanged()
+	 * For DialogFragment, call it on onViewCreated()
+	 */
+	public static void toImmersiveMode(Window window) {
+		if (window != null) {
+			window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+			window.getDecorView().setSystemUiVisibility(
+					View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+							| View.SYSTEM_UI_FLAG_FULLSCREEN
+							| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+							| View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+							| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+							| View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+			);
+		}
 	}
 }
