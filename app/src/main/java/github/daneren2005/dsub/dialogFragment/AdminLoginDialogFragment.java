f5c09c2c00f3b0c9747f5f8b53a03f740d2b0228
YC <aa>
2020-07-16 03:31:49 +0800

    Notify-010: Jump to setting

--- app/src/main/java/github/daneren2005/dsub/dialogFragment/AdminLoginDialogFragment.java
+++ app/src/main/java/github/daneren2005/dsub/dialogFragment/AdminLoginDialogFragment.java
@@ -1,5 +1,6 @@
 package github.daneren2005.dsub.dialogFragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -11,8 +12,10 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import github.daneren2005.dsub.R;
+import github.daneren2005.dsub.activity.SettingsActivity;
+import github.daneren2005.dsub.activity.SubsonicActivity;
 
-public class AdminLoginDialogFragment extends NotifyDialogFragmentTemplate {
+public class AdminLoginDialogFragment extends NotifyDialogFragmentTemplate implements View.OnClickListener {
     private static final String TAG = AdminLoginDialogFragment.class.getSimpleName();
     private static final String ADMIN_PASSWORD = "p@ssword";
 
@@ -25,17 +28,21 @@ public class AdminLoginDialogFragment extends NotifyDialogFragmentTemplate {
 
         passwordEdt = view.findViewById(R.id.notify_dialog_input_edt);
         Button enterBtn = view.findViewById(R.id.notify_dialog_bottom_btn);
-        enterBtn.setOnClickListener(v -> {
-            if (passwordEdt.getText().toString().equals(ADMIN_PASSWORD)) {
-                // TODO: Enter Setting Page
-                Toast.makeText(this.getContext(), "Correct password!", Toast.LENGTH_SHORT).show();
-                this.dismiss();
-            } else {
-                passwordEdt.setText(null);
-                Toast.makeText(this.getContext(), "Wrong password!", Toast.LENGTH_SHORT).show();
-            }
-        });
+        enterBtn.setOnClickListener(this);
 
         return view;
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (passwordEdt.getText().toString().equals(ADMIN_PASSWORD)) {
+            // TODO: Enter Setting Page
+            Toast.makeText(this.getContext(), "Correct password!", Toast.LENGTH_SHORT).show();
+            this.dismiss();
+            startActivity(new Intent(getContext(), SettingsActivity.class));
+        } else {
+            passwordEdt.setText(null);
+            Toast.makeText(this.getContext(), "Wrong password!", Toast.LENGTH_SHORT).show();
+        }
     }
 }
